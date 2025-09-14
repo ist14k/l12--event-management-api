a682407ff6c60c8a1f2bbfb194ace9583ba78e45
ist14k <istiak_akash@outlook.com>
2025-09-14 23:34:20 +0600

    refactor: add type hints for user and attendees relationships in Event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
@@ -17,12 +19,12 @@ class Event extends Model
         'end_time',
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function attendees()
+    public function attendees(): HasMany
     {
         return $this->hasMany(Attendee::class);
     }
