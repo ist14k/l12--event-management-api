9884803e0af13e3886e50bc54f005e8835f1a10d
ist14k <istiak_akash@outlook.com>
2025-09-14 23:19:02 +0600

    feat: enhance Event and Attendee resources for better data representation

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(Event::with(['user'])->get());
     }
 
     /**
@@ -30,7 +31,7 @@ class EventController extends Controller
 
         $event = Event::create(array_merge($validated, ['user_id' => 1]));
 
-        return response()->json($event, 201);
+        return new EventResource($event);
     }
 
     /**
@@ -38,7 +39,7 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        return new EventResource($event->load(['user', 'attendees']));
     }
 
     /**
@@ -55,7 +56,7 @@ class EventController extends Controller
 
         $event->update($validated);
 
-        return response()->json($event);
+        return new EventResource($event);
     }
 
     /**
