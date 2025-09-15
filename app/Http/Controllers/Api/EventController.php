3b0aa59a2dfecddecf748446cfab817b34d86d65
ist14k <istiak_akash@outlook.com>
2025-09-15 22:48:28 +0600

    fix: add constructor to EventController for middleware authentication

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
     /**
      * Display a listing of the resource.
      */
